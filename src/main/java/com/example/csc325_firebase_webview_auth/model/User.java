07889abf3d895b1f8886ebdfb3e52973bc9f809d
WJijon223 <williamjijon223@gmail.com>
2026-03-12 21:37:15 -0400

    created User model

---
+++ src/main/java/com/example/csc325_firebase_webview_auth/model/User.java
@@ -0,0 +1,36 @@
+package com.example.csc325_firebase_webview_auth.model;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
